5ab81b0ecaf424efc39c08e5a0ef945f9c46a982
Musa Khan <57375561+MusaMoses@users.noreply.github.com>
2021-12-09 10:08:09 -0500

    backend progress

--- functions/components.js
+++ functions/components.js
@@ -1,6 +1,15 @@
+import mongoose from 'mongoose';
+const { CPU } = require('../db_config/component_models.js')
+
 exports.handler = async (event, context) => {
+
+    console.log(process.env.URI)
+    console.log(event.body)
+    const data = CPU(JSON.parse(event.body))
+    data.save()
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ 'message': 'GTX 1050TI' })
+        body: event.body
     }
 };
